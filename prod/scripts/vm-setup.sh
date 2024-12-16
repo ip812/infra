06da9f0cfbdd2b30138b5520dfa6cb51bb785a59
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-12-16 10:35:51 +0200

    chore: add commnts in vm-setup.sh

--- prod/scripts/vm-setup.sh
+++ prod/scripts/vm-setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+# SSH keys
 echo ${admin_ssh_public_key} >> /home/ubuntu/.ssh/authorized_keys
 echo ${deploy_ssh_public_key} >> /home/ubuntu/.ssh/authorized_keys
 
+# Dependencies
 apt-get update -y
 apt-get install -y tmux vim stow
+
+# dotfiles
 cd /home/ubuntu
 git clone https://github.com/iypetrov/.vm-dotfiles.git
 (cd .vm-dotfiles && stow .)
 (cd .vm-dotfiles && stow -d /root/.vm-dotfiles .)
 chmod -R ugo+r /home/ubuntu/.vm-dotfiles
 
+# Docker
 curl -fsSl https://get.docker.com | sh
 gpasswd -a ubuntu docker
 
+# Swarm
 docker swarm init
 curl -X POST \
   -H "Accept: application/vnd.github+json" \
