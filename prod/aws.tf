6b709304addbb3bec011207ac17c4bd6364f6037
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-02 14:57:35 +0200

    drop not needed secret

--- prod/aws.tf
+++ prod/aws.tf
@@ -172,7 +172,3 @@ resource "aws_secretsmanager_secret_version" "blog_db_file_version" {
   secret_id     = aws_secretsmanager_secret.blog_db_file.id
   secret_string = var.blog_db_file
 }
-
-resource "aws_secretsmanager_secret" "blog_port" {
-  name            = "blog_port"
-}
