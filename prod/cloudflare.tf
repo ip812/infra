5f40e91761ca871e9c89fd7a4ec4942499fc22a0
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-05 21:55:06 +0200

    fix: change names

--- prod/cloudflare.tf
+++ prod/cloudflare.tf
@@ -1,13 +1,4 @@
-resource "cloudflare_dns_record" "blog_dns_record" {
-  zone_id = var.cloudflare_blog_zone_id
-  name    = var.blog_domain
-  content = aws_eip.eip.public_ip
-  type    = "A"
-  ttl     = 1
-  proxied = true
-}
-
-resource "cloudflare_dns_record" "blog_dns_record" {
+resource "cloudflare_dns_record" "traefik_dns_record" {
   zone_id = var.cloudflare_blog_zone_id
   name    = "traefik"
   content = aws_eip.eip.public_ip
@@ -16,9 +7,18 @@ resource "cloudflare_dns_record" "blog_dns_record" {
   proxied = true
 }
 
-resource "cloudflare_dns_record" "blog_dns_record" {
+resource "cloudflare_dns_record" "portainer_dns_record" {
   zone_id = var.cloudflare_blog_zone_id
   name    = "portainer"
+  content = aws_eip.eip.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "blog_dns_record" {
+  zone_id = var.cloudflare_blog_zone_id
+  name    = var.blog_domain
   content = aws_eip.eip.public_ip
   type    = "A"
   ttl     = 1
