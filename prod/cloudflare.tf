5af13ea5c66d120566ad4b09e86076826438dd8e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-05 21:53:25 +0200

    feat: add dns records for traefik and portainer

--- prod/cloudflare.tf
+++ prod/cloudflare.tf
@@ -6,3 +6,21 @@ resource "cloudflare_dns_record" "blog_dns_record" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_dns_record" "blog_dns_record" {
+  zone_id = var.cloudflare_blog_zone_id
+  name    = "traefik"
+  content = aws_eip.eip.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "blog_dns_record" {
+  zone_id = var.cloudflare_blog_zone_id
+  name    = "portainer"
+  content = aws_eip.eip.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
