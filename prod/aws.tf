851f087337c63d0c3e5596a4aebc97403db8d33b
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-05 23:56:47 +0200

    more sg

--- prod/aws.tf
+++ prod/aws.tf
@@ -115,6 +115,28 @@ resource "aws_security_group" "vm_sg" {
       protocol         = "tcp"
       security_groups  = []
       self             = false
+      to_port          = 8000
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "https"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 9000
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "https"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
       to_port          = 9443
     },
   ]
