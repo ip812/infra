3130ebeab8e97b37c28a60bd0bf18ad8ad90cdde
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-06 09:48:29 +0200

    add ap

--- prod/cloudflare.tf
+++ prod/cloudflare.tf
@@ -7,6 +7,25 @@ resource "cloudflare_dns_record" "traefik_dns_record" {
   proxied = true
 }
 
+resource "cloudflare_access_application" "traefik_ap" {
+  zone_id          = var.cloudflare_blog_zone_id
+  name             = "Traefik"
+  domain           = "traefik.deviliablog.com"
+  session_duration = "1h"
+}
+
+resource "cloudflare_access_policy" "traefik_policy" {
+  application_id = cloudflare_access_application.traefik_ap.id
+  zone_id        = var.cloudflare_blog_zone_id
+  name           = "Traefik Access Policy"
+  precedence     = 1
+  decision       = "allow"
+
+  include {
+    emails = ["iliayavorovpetrov@gmail.com"]
+  }
+}
+
 resource "cloudflare_dns_record" "portainer_dns_record" {
   zone_id = var.cloudflare_blog_zone_id
   name    = "portainer"
@@ -14,6 +33,25 @@ resource "cloudflare_dns_record" "portainer_dns_record" {
   type    = "A"
   ttl     = 1
   proxied = true
+}
+
+resource "cloudflare_access_application" "portainer_ap" {
+  zone_id          = var.cloudflare_blog_zone_id
+  name             = "Portainer"
+  domain           = "portainer.deviliablog.com"
+  session_duration = "1h"
+}
+
+resource "cloudflare_access_policy" "portainer_policy" {
+  application_id = cloudflare_access_application.portainer_ap.id
+  zone_id        = var.cloudflare_blog_zone_id
+  name           = "Portainer Access Policy"
+  precedence     = 1
+  decision       = "allow"
+
+  include {
+    emails = ["iliayavorovpetrov@gmail.com"]
+  }
 }
 
 resource "cloudflare_dns_record" "blog_dns_record" {
