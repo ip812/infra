056861b2b66e52367728641366f899ffae9a02c8
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-07 09:56:44 +0200

    drop ports 80 and 443

--- prod/aws.tf
+++ prod/aws.tf
@@ -84,28 +84,6 @@ resource "aws_security_group" "vm_sg" {
       self             = false
       to_port          = 22
     },
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = "http"
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 80
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = "https"
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 443
-    },
   ]
   egress = [
     {
