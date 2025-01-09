effe1d7503f9a808fbf92622eea7a3a1cbcd68d6
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-01-09 13:54:37 +0200

    adjust egress rules

--- prod/aws.tf
+++ prod/aws.tf
@@ -76,14 +76,14 @@ resource "aws_security_group" "vm_sg" {
   egress = [
     {
       cidr_blocks      = ["0.0.0.0/0"]
-      description      = "ssm"
+      description      = "session manager"
       from_port        = 0
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
-      protocol         = "tcp"
+      protocol         = -1
       security_groups  = []
       self             = false
-      to_port          = 443
+      to_port          = 0
     },
   ]
   tags = {
