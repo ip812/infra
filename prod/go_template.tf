07558bf2bd9af6e495a3c94ac394e795087a6bdd
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-03-17 10:22:58 +0200

    fix: add check if the database already exists db-query-exec Lambda function

--- prod/go_template.tf
+++ prod/go_template.tf
@@ -41,16 +41,15 @@ resource "cloudflare_record" "go_template_dns_record" {
 ################################################################################
 #                                     DB                                       #
 ################################################################################
-
 resource "aws_lambda_invocation" "create_go_template_db" {
   depends_on = [
     aws_lambda_function.db_query_exec_function
   ]
 
   function_name = aws_lambda_function.db_query_exec_function.function_name
-  input         = jsonencode({
-    database_name: "postgres",
-    query: "CREATE DATABASE \"${var.go_template_db_name}\";",
-    ssl_mode: "require"
+  input = jsonencode({
+    database_name = "postgres",
+    query = "SELECT 'CREATE DATABASE \"${var.go_template_db_name}\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${var.go_template_db_name}');",
+    ssl_mode = "require"
   })
 }
