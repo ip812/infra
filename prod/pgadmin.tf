078f6d7099b40adf2d453b3ae32f55837c195577
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-03-17 14:17:10 +0200

    feat: secure the pgadmin.ip812.com

--- prod/pgadmin.tf
+++ prod/pgadmin.tf
@@ -19,3 +19,23 @@ resource "cloudflare_record" "pgadmin_dns_record" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_zero_trust_access_application" "pgadmin_zt_app" {
+  zone_id                   = var.cloudflare_ip812_zone_id
+  name                      = "pgadmin"
+  domain                    = "pgadmin.${var.org}.com"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  auto_redirect_to_identity = true
+}
+
+resource "cloudflare_zero_trust_access_policy" "pgadmin_ap" {
+  zone_id        = var.cloudflare_ip812_zone_id
+  application_id = cloudflare_zero_trust_access_application.pgadmin_zt_app.id
+  name           = "pgadmin"
+  decision       = "allow"
+  precedence     = 1
+  include {
+    email = var.whitelist_email_addresses
+  }
+}
