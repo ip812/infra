9fb7f632b29035525679046b99cc8c6ecb48dede
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-03-21 07:30:04 +0200

    feat: rename lambda function ecr from db-query-exec to pg-query-exec + add ecr for go-ssr

--- common/ecr.tf
+++ common/ecr.tf
@@ -1,4 +1,12 @@
 ################################################################################
+#                                    Common                                    #
+################################################################################
+
+resource "aws_ecr_repository" "go_ssr_ecr_repository" {
+  name                 = "${data.terraform_remote_state.prod.outputs.org}/go-ssr"
+  image_tag_mutability = "IMMUTABLE"
+}
+################################################################################
 #                                   Services                                   #
 ################################################################################
 
@@ -16,7 +24,7 @@ resource "aws_ecr_repository" "hello_ecr_repository" {
   image_tag_mutability = "IMMUTABLE"
 }
 
-resource "aws_ecr_repository" "db_query_exec_ecr_repository" {
-  name                 = "${data.terraform_remote_state.prod.outputs.org}/db-query-exec"
+resource "aws_ecr_repository" "pg_query_exec_ecr_repository" {
+  name                 = "${data.terraform_remote_state.prod.outputs.org}/pg-query-exec"
   image_tag_mutability = "IMMUTABLE"
 }
