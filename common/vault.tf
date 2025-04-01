7cbcf3f0af71645d8bdfe8ccca59ea95d49aff17
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-01 08:24:18 +0300

    feat: move all aws credentials to a single secret

--- common/vault.tf
+++ common/vault.tf
@@ -1,5 +1,9 @@
-resource "hcp_vault_secrets_secret" "prod_aws_access_key" {
-  app_name     = var.env_prod 
-  secret_name  = "aws_access_key"
-  secret_value = data.terraform_remote_state.prod.outputs.aws_access_key
+resource "hcp_vault_secrets_secret" "prod_aws_credentials" {
+  app_name     = var.env_prod
+  secret_name  = "aws_credentials"
+  secret_value = jsonencode({
+    access_key = data.terraform_remote_state.prod.outputs.aws_access_key
+    secret_key = data.terraform_remote_state.prod.outputs.aws_secret_key
+    region     = data.terraform_remote_state.prod.outputs.aws_region
+  })
 }
