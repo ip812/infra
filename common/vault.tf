fc35163cb2f2e155437efeed42bc70c5eef21d51
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-01 13:33:11 +0300

    feat: split each vault secret into different resource

--- common/vault.tf
+++ common/vault.tf
@@ -1,19 +1,35 @@
-resource "hcp_vault_secrets_secret" "prod_aws_credentials" {
+resource "hcp_vault_secrets_secret" "prod_aws_access_key" {
   app_name     = var.env_prod
   secret_name  = "aws"
-  secret_value = jsonencode({
-    access_key = data.terraform_remote_state.prod.outputs.aws_access_key
-    secret_key = data.terraform_remote_state.prod.outputs.aws_secret_key
-    region     = data.terraform_remote_state.prod.outputs.aws_region
-  })
+  secret_value = data.terraform_remote_state.prod.outputs.aws_access_key
 }
 
-resource "hcp_vault_secrets_secret" "prod_pg_credentials" {
+resource "hcp_vault_secrets_secret" "prod_aws_secret_key" {
   app_name     = var.env_prod
-  secret_name  = "pg"
-  secret_value = jsonencode({
-    pg_endpoint = data.terraform_remote_state.prod.outputs.pg_endpoint
-    pg_username = data.terraform_remote_state.prod.outputs.pg_username
-    pg_password = data.terraform_remote_state.prod.outputs.pg_password
-  })
+  secret_name  = "aws"
+  secret_value = data.terraform_remote_state.prod.outputs.aws_secret_key
+}
+
+resource "hcp_vault_secrets_secret" "prod_aws_region" {
+  app_name     = var.env_prod
+  secret_name  = "aws"
+  secret_value = data.terraform_remote_state.prod.outputs.aws_region
+}
+
+resource "hcp_vault_secrets_secret" "prod_pg_endpoint" {
+  app_name     = var.env_prod
+  secret_name  = "pg_endpoint"
+  secret_value = data.terraform_remote_state.prod.outputs.pg_endpoint
+}
+
+resource "hcp_vault_secrets_secret" "prod_pg_username" {
+  app_name     = var.env_prod
+  secret_name  = "pg_username"
+  secret_value = data.terraform_remote_state.prod.outputs.pg_username
+}
+
+resource "hcp_vault_secrets_secret" "prod_pg_password" {
+  app_name     = var.env_prod
+  secret_name  = "pg+password"
+  secret_value = data.terraform_remote_state.prod.outputs.pg_password
 }
