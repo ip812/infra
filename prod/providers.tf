0f67d4fd2a013a2882c746c317083479acf89c7d
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-14 10:51:48 +0300

    fix: add missing config awscc

--- prod/providers.tf
+++ prod/providers.tf
@@ -119,9 +119,13 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+provider "awscc" {
+  region     = var.aws_region
 }
 
 provider "cloudflare" {
