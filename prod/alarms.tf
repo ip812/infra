40148a094b12b2b08686e0603f7348e029d736cf
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-14 11:28:41 +0300

    feat: add 2 alarms for the rds

--- prod/alarms.tf
+++ prod/alarms.tf
@@ -22,6 +22,52 @@ resource "aws_cloudwatch_metric_alarm" "asg_high_cpu_alarm" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "rds_high_cpu_alarm" {
+  alarm_name          = "rds-high-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 90
+  actions_enabled     = true
+  alarm_description   = "Alarm when RDS CPU exceeds max threshold"
+  alarm_actions = [
+    aws_sns_topic.alarms_topic.arn
+  ]
+  ok_actions = [
+    aws_sns_topic.alarms_topic.arn
+  ]
+  insufficient_data_actions = []
+  dimensions = {
+     DBInstanceIdentifier = aws_db_instance.pg.id
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_low_storage_alarm" {
+  alarm_name          = "rds-low-storage-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = 60
+  statistic           = "Minimum"
+  threshold           = 2147483648 # 2 GB
+  actions_enabled     = true
+  alarm_description   = "Alarm when RDS free storage is less than 2 GB"
+  alarm_actions = [
+    aws_sns_topic.alarms_topic.arn
+  ]
+  ok_actions = [
+    aws_sns_topic.alarms_topic.arn
+  ]
+  insufficient_data_actions = []
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.pg.id
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "asg_two_instances_alarm" {
   alarm_name                = "asg-two-instances-alarm"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
