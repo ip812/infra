bb60da512ea2f53690212d24a1128d235c506575
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-24 10:12:54 +0300

    chore: add test fodler

--- prod/grafana.tf
+++ prod/grafana.tf
@@ -2,3 +2,8 @@ resource "grafana_folder" "test_folder" {
   provider = grafana.stack
   title    = "Test Folder 1"
 }
+
+resource "grafana_folder" "foo_folder" {
+  provider = grafana.stack
+  title    = "Foo Folder 1"
+}
