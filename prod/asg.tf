376febdfe036327d66df829964bfeefb9974bb58
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-02 08:14:33 +0300

    fix: remove not needed helm upgrade

--- prod/asg.tf
+++ prod/asg.tf
@@ -123,11 +123,6 @@ resource "aws_launch_template" "asg_lt" {
     k0s kubectl create namespace monitoring
     helm repo add grafana https://grafana.github.io/helm-charts
     helm repo update
-    helm search repo grafana
-    helm upgrade --install --version ^2 --atomic --timeout 300s grafana-k8s-monitoring grafana/k8s-monitoring \
-        --namespace "monitoring" \
-        --values apps/values/monitoring.yaml
-
     helm upgrade --install --version ^2 --atomic --timeout 300s grafana-k8s-monitoring grafana/k8s-monitoring \
       --namespace "monitoring" \
       --values - <<-VALUES
