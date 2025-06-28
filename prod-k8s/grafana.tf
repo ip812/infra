5297c1d20b68dd86a9d8439bce3cf4ea015a0615
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-29 00:26:50 +0300

    feat: pin version grafana-k8s-monitoring helm release
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/grafana.tf
+++ prod-k8s/grafana.tf
@@ -14,6 +14,6 @@ resource "helm_release" "grafana_k8s_monitoring" {
   name       = "grafana-k8s-monitoring"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "k8s-monitoring"
-  version    = "^2.0.0"
+  version    = "2.1.4"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 }
