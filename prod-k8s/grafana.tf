9859dac56690a5e65735a99ac5702d3c1126f4e0
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-29 23:54:52 +0300

    fix: add missing ""
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/grafana.tf
+++ prod-k8s/grafana.tf
@@ -103,7 +103,7 @@ alloy-logs:
     url: ${data.terraform_remote_state.prod.outputs.gf_cloud_stack.fleet_management_url} 
     auth:
       type: basic
-      username: ${data.terraform_remote_state.prod.outputs.gf_cloud_stack.profiles_user_id} 
+      username: "${data.terraform_remote_state.prod.outputs.gf_cloud_stack.profiles_user_id}"
       password: ${var.gf_cloud_access_policy_token}
 alloy-singleton:
   enabled: false
