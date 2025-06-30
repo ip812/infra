419a33fdb46c39e2897dcf9c13f11dea60b63896
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-30 15:18:36 +0300

    feat: drop postgres namespace
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/postgres.tf
+++ prod-k8s/postgres.tf
@@ -1,5 +1,10 @@
-resource "kubernetes_namespace" "database" {
-  metadata {
-    name = "database"
-  }
-}
+# resource "helm_release" "grafana_k8s_monitoring" {
+#   name             = "grafana-k8s-monitoring"
+#   repository       = "https://grafana.github.io/helm-charts"
+#   chart            = "k8s-monitoring"
+#   version          = "2.1.4"
+#   namespace        = ""
+#   create_namespace = true
+#   wait             = false
+#   timeout          = 600
+# }
