780d83c45bb2805a707db51af6a3d16001fc3195
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-30 15:22:15 +0300

    feat: upgrade grafana helm chart to 3.1.0
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/grafana.tf
+++ prod-k8s/grafana.tf
@@ -8,7 +8,7 @@ resource "helm_release" "grafana_k8s_monitoring" {
   name             = "grafana-k8s-monitoring"
   repository       = "https://grafana.github.io/helm-charts"
   chart            = "k8s-monitoring"
-  version          = "2.1.4"
+  version          = "3.1.0"
   namespace        = "monitoring"
   create_namespace = true
   wait             = false
