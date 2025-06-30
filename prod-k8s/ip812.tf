8d625ca96efa6b159618909161b64edb7a1f7407
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-06-30 17:17:59 +0300

    feat: add simple single node postgres db
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/ip812.tf
+++ prod-k8s/ip812.tf
@@ -3,3 +3,11 @@ resource "kubernetes_namespace" "ip812" {
     name = var.org
   }
 }
+
+resource "helm_release" "app_pg" {
+  name       = "app-pg"
+  namespace  = kubernetes_namespace.ip812.metadata[0].name
+  chart      = "${path.module}/charts/app-pg"
+  repository = ""
+  version = "0.1.0"
+}
