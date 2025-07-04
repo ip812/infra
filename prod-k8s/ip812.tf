bb0fd44b00e23062e1ce811764df304efcfb0f6f
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-04 12:58:51 +0300

    feat: add dummy logic to update helm charts
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/ip812.tf
+++ prod-k8s/ip812.tf
@@ -4,16 +4,31 @@ resource "kubernetes_namespace" "ip812" {
   }
 }
 
-resource "helm_release" "app_pg" {
-  depends_on = [
-    kubernetes_namespace.ip812
+data "external" "chart_hash" {
+  program = ["bash", "-c", <<-EOT
+    find "${path.module}/charts/app-pg" -type f -print0 \
+    | sort -z \
+    | xargs -0 sha256sum \
+    | sha256sum \
+    | jq -Rn '{"hash": input}'
+  EOT
   ]
+}
+
+resource "helm_release" "app_pg" {
+  depends_on = [kubernetes_namespace.ip812]
+
   name       = "app-pg"
   namespace  = kubernetes_namespace.ip812.metadata[0].name
   chart      = "${path.module}/charts/app-pg"
   repository = ""
-  version = "0.1.0"
-  force_update = true
-  wait             = false
-  timeout          = 600
+  version    = "0.1.0"
+  wait          = false
+  timeout       = 600
+
+  # dummy value to trigger update on chart changes
+  set {
+    name  = "chartContentHash"
+    value = trimspace(data.external.chart_hash.result["hash"])
+  }
 }
