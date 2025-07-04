3fea83798245d815a55ee27d9b38331fc7309c3a
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-04 11:51:48 +0300

    feat: adjust the cnpg config so to put backups in s3
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/ip812.tf
+++ prod-k8s/ip812.tf
@@ -1,13 +1,13 @@
-# resource "kubernetes_namespace" "ip812" {
-#   metadata {
-#     name = var.org
-#   }
-# }
-# 
-# resource "helm_release" "app_pg" {
-#   name       = "app-pg"
-#   namespace  = kubernetes_namespace.ip812.metadata[0].name
-#   chart      = "${path.module}/charts/app-pg"
-#   repository = ""
-#   version = "0.1.0"
-# }
+resource "kubernetes_namespace" "ip812" {
+  metadata {
+    name = var.org
+  }
+}
+
+resource "helm_release" "app_pg" {
+  name       = "app-pg"
+  namespace  = kubernetes_namespace.ip812.metadata[0].name
+  chart      = "${path.module}/charts/app-pg"
+  repository = ""
+  version = "0.1.0"
+}
