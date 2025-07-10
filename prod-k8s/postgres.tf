7b85b5c8ff4f5686771f5a59c3f251628e3455f2
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-10 23:01:02 +0300

    fix: adjust version cnpg to the latest helm chart
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod-k8s/postgres.tf
+++ prod-k8s/postgres.tf
@@ -2,7 +2,7 @@ resource "helm_release" "cnpg_cloudnative_pg" {
   name             = "cnpg-cloudnative-pg"
   repository       = "https://cloudnative-pg.github.io/charts"
   chart            = "cloudnative-pg"
-  version          = "v1.26.0"
+  version          = "0.24.0"
   namespace        = "databases"
   create_namespace = true
   wait             = true
