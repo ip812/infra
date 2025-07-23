1fb2eabaac8515e434a729e5cd1b4e0200cff56a
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-23 21:10:35 +0300

    feat: remove cloudflare resources for now
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod/s3.tf
+++ prod/s3.tf
@@ -1,5 +1,9 @@
-resource "cloudflare_r2_bucket" "app_s3" {
+resource "cloudflare_r2_bucket" "go_template_bucket" {
   account_id = var.cf_account_id
   name       = "${var.org}-${var.go_template_db_name}-bucket"
   location   = "EEUR"
+}
+
+output "go_template_bucket_endpoint" {
+  value = cloudflare_r2_bucket.go_template_bucket.id
 }
