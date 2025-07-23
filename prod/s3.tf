c1689d72c163a1d7dc10ec605d4522e22245c03e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-23 22:36:55 +0300

    feat: drop cloudflare workers, not needed
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod/s3.tf
+++ prod/s3.tf
@@ -8,17 +8,6 @@ output "go_template_bucket_endpoint" {
   value = cloudflare_r2_bucket.go_template_bucket.name
 }
 
-resource "cloudflare_workers_script" "go_template_serve_files" {
-  account_id = var.cf_account_id
-  name       = "go-template-serve-files"
-  content    = file("${path.module}/scripts/serve_files.js")
-
-  r2_bucket_binding {
-    name        = "FILES_BUCKET"
-    bucket_name = cloudflare_r2_bucket.go_template_bucket.name
-  }
-}
-
 resource "cloudflare_r2_bucket" "blog_bucket" {
   account_id = var.cf_account_id
   name       = "${var.org}-${var.blog_db_name}-bucket"
@@ -27,15 +16,4 @@ resource "cloudflare_r2_bucket" "blog_bucket" {
 
 output "blog_bucket_endpoint" {
   value = cloudflare_r2_bucket.blog_bucket.name
-}
-
-resource "cloudflare_workers_script" "blog_serve_files" {
-  account_id = var.cf_account_id
-  name       = "blog-serve-files"
-  content    = file("${path.module}/scripts/serve_files.js")
-
-  r2_bucket_binding {
-    name        = "FILES_BUCKET"
-    bucket_name = cloudflare_r2_bucket.blog_bucket.name
-  }
 }
