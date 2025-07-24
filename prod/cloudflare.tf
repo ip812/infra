1c1b936bf087ba5ef60039d322554ba037a09930
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-24 13:28:45 +0300

    feat: use other hsotname
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod/cloudflare.tf
+++ prod/cloudflare.tf
@@ -19,7 +19,7 @@ resource "cloudflare_dns_record" "blog_dns_record" {
 }
 
 output "blog_hostname" {
-  value = cloudflare_dns_record.blog_dns_record.meta
+  value = "${cloudflare_dns_record.blog_dns_record.name}.${var.org}.com"
 }
 
 resource "cloudflare_dns_record" "go_template_dns_record" {
@@ -32,5 +32,5 @@ resource "cloudflare_dns_record" "go_template_dns_record" {
 }
 
 output "go_template_hostname" {
-  value = cloudflare_dns_record.go_template_dns_record.id
+  value = "${cloudflare_dns_record.go_template_dns_record.name}.${var.org}.com"
 }
