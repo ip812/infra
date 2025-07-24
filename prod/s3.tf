61ede21b1eb1a446ac5d18f1152a32dc512428a6
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-24 16:22:35 +0300

    fix: adjsut the domain 2
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- prod/s3.tf
+++ prod/s3.tf
@@ -12,7 +12,7 @@ output "go_template_bucket_endpoint" {
 resource "cloudflare_r2_custom_domain" "go_template_bucket_custom_domain" {
   account_id  = var.cf_account_id
   bucket_name = cloudflare_r2_bucket.go_template_bucket.name
-  domain      = "static"
+  domain      = "${var.blog_domain}.${var.org}.com"
   enabled     = true
   zone_id     = var.cf_ip812_zone_id
   min_tls     = "1.0"
