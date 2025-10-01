223b7b30ccd63dd2e573e3ca8d5aa89d4a3d61d8
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-10-01 18:40:51 +0300

    fix: adjust the droppler config
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/droppler.tf
+++ terraform/prod/droppler.tf
@@ -1,6 +1,6 @@
 resource "doppler_secret" "gf_cloud_access_policy_token" {
   project = "prod"
-  config  = "prod"
+  config  = "prd"
   name    = "GF_CLOUD_ACCESS_POLICY_TOKEN"
   value   = var.gf_cloud_access_policy_token
 }
