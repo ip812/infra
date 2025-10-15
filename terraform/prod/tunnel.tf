1693a34e30be01b2d4f9354dcb3321981d2bc9e7
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-10-15 13:55:38 +0300

    feat: add ingess for blog app
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/tunnel.tf
+++ terraform/prod/tunnel.tf
@@ -8,6 +8,10 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cf_tunnel_cfg" {
         service  = "http://${local.go_template_app_name}-svc.${local.go_template_app_name}.svc.cluster.local:8080"
       },
       {
+        hostname = cloudflare_dns_record.blog_dns_record.name
+        service  = "http://${local.blog_app_name}-svc.${local.blog_app_name}.svc.cluster.local:8080"
+      },
+      {
         service = "http_status:404"
       }
     ]
