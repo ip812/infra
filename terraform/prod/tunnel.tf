2d50df711ee5f6ec579ff808d7a6c5da076d524e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-10-15 10:49:10 +0300

    feat: add tunnel config
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ terraform/prod/tunnel.tf
@@ -0,0 +1,15 @@
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cf_tunnel_cfg" {
+  account_id = var.cf_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id
+  config = {
+    ingress = [
+      {
+        hostname = cloudflare_dns_record.go_template_dns_record.name
+        service  = "http://${local.go_template_app_name}-svc.${local.go_template_app_name}.svc.cluster.local:8080"
+      },
+      {
+        service = "http_status:404"
+      }
+    ]
+  }
+}
