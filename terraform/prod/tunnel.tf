f2a2307774620e6621f3c51da145de29c2c7ccb3
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-12-16 17:30:57 +0200

    feat: add 2 more private domains
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/tunnel.tf
+++ terraform/prod/tunnel.tf
@@ -3,30 +3,30 @@ locals {
     "ilia.yavorov.petrov@gmail.com"
   ]
   config = {
-    template = { 
-      k8s_ns = "template", 
-      k8s_svc_name = "template-svc", 
-      k8s_svc_port = "8080", 
-      is_protected = false 
+    template = {
+      k8s_ns       = "template",
+      k8s_svc_name = "template-svc",
+      k8s_svc_port = "8080",
+      is_protected = false
     }
-    blog = { 
-      k8s_ns = "blog", 
-      k8s_svc_name = "blog-svc", 
-      k8s_svc_port = "8080", 
-      is_protected = false 
+    blog = {
+      k8s_ns       = "blog",
+      k8s_svc_name = "blog-svc",
+      k8s_svc_port = "8080",
+      is_protected = false
     }
-    # pgadmin = { 
-    #   k8s_ns = "database", 
-    #   k8s_svc_name = "pgadmin-svc", 
-    #   k8s_svc_port = "8080", 
-    #   is_protected = true 
-    # }
-    # capacitor = { 
-    #   k8s_ns = "flux-system", 
-    #   k8s_svc_name = "capacitor", 
-    #   k8s_svc_port = "9000", 
-    #   is_protected = true
-    # }
+    pgadmin = {
+      k8s_ns       = "database",
+      k8s_svc_name = "pgadmin-svc",
+      k8s_svc_port = "8080",
+      is_protected = true
+    }
+    capacitor = {
+      k8s_ns       = "flux-system",
+      k8s_svc_name = "capacitor",
+      k8s_svc_port = "9000",
+      is_protected = true
+    }
   }
 }
 
@@ -43,13 +43,13 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "cf_tunnel_token" {
 }
 
 resource "cloudflare_dns_record" "dns_record" {
-  zone_id = var.cf_ip812_zone_id
   for_each = local.config
-  name    = "${each.key}.${local.org}.com" 
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
+  zone_id  = var.cf_ip812_zone_id
+  name     = "${each.key}.${local.org}.com"
+  content  = "${cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id}.cfargotunnel.com"
+  type     = "CNAME"
+  ttl      = 1
+  proxied  = true
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cf_tunnel_cfg" {
@@ -73,40 +73,39 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cf_tunnel_cfg" {
   }
 }
 
-# resource "cloudflare_dns_record" "blog_dns_record" {
-#   zone_id = var.cf_ip812_zone_id
-#   name    = "${local.blog_domain}.${local.org}.com"
-#   content = "${cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id}.cfargotunnel.com"
-#   type    = "CNAME"
-#   ttl     = 1
-#   proxied = true
-# }
-# 
-# resource "cloudflare_dns_record" "go_template_dns_record" {
-#   zone_id = var.cf_ip812_zone_id
-#   name    = "${local.go_template_domain}.${local.org}.com"
-#   content = "${cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id}.cfargotunnel.com"
-#   type    = "CNAME"
-#   ttl     = 1
-#   proxied = true
-# }
-# 
-# resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cf_tunnel_cfg" {
-#   account_id = var.cf_account_id
-#   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cf_tunnel.id
-#   config = {
-#     ingress = [
-#       {
-#         hostname = cloudflare_dns_record.go_template_dns_record.name
-#         service  = "http://${local.go_template_app_name}-svc.${local.go_template_app_name}.svc.cluster.local:8080"
-#       },
-#       {
-#         hostname = cloudflare_dns_record.blog_dns_record.name
-#         service  = "http://${local.blog_app_name}-svc.${local.blog_app_name}.svc.cluster.local:8080"
-#       },
-#       {
-#         service = "http_status:404"
-#       }
-#     ]
-#   }
-# }
+resource "cloudflare_zero_trust_access_policy" "zt_access_policy" {
+  account_id       = var.cf_account_id
+  name             = "Admin allowlist"
+  decision         = "allow"
+  session_duration = "8h"
+
+  include = [
+    for email in local.whitelist_emails : {
+      email = {
+        email = email
+      }
+    }
+  ]
+}
+
+resource "cloudflare_zero_trust_access_application" "zt_access_application" {
+  for_each = {
+    for key, cfg in local.config :
+    key => cfg
+    if cfg.is_protected
+  }
+
+  account_id                = var.cf_account_id
+  name                      = "Prod Avalon Backoffice"
+  domain                    = "${each.key}.${local.org}.com"
+  type                      = "self_hosted"
+  session_duration          = "8h"
+  auto_redirect_to_identity = true
+
+  policies = [
+    {
+      id         = cloudflare_zero_trust_access_policy.zt_access_policy.id
+      precedence = 1
+    }
+  ]
+}
