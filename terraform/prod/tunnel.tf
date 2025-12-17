0c325c9619b0ed06ebccc84d212a16d3881189cc
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-12-17 11:14:33 +0200

    feat: expose prometheus
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/tunnel.tf
+++ terraform/prod/tunnel.tf
@@ -33,6 +33,12 @@ locals {
       k8s_svc_port = "80",
       is_protected = true
     }
+    prometheus = {
+      k8s_ns       = "monitoring",
+      k8s_svc_name = "kube-prometheus-stack-prometheus",
+      k8s_svc_port = "9090",
+      is_protected = true
+    }
     jaeger = {
       k8s_ns       = "monitoring",
       k8s_svc_name = "jaeger-query",
