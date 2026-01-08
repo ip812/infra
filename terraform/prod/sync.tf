df40a4bc7aaff647c885ae4be75e29aff3aeae2f
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-01-08 14:34:27 +0200

    feat: add otel envs for blog
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/sync.tf
+++ terraform/prod/sync.tf
@@ -118,6 +118,10 @@ env:
       secretKeyRef:
         name: "${local.blog_app_name}-creds" 
         key: SLACK_BLOG_BOT_TOKEN
+  - name: OTEL_EXPORTER_OTLP_ENDPOINT
+    value: "http://jaeger.${local.tunnel_config.jaeger.k8s_ns}.svc.cluster.local:4318"
+  - name: OTEL_EXPORTER_OTLP_INSECURE
+    value: "true"
 database:
   postgres:
     enabled: true
