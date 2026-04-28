81903ea699cabbde45b9f72265a61015caa49512
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-04-28 12:21:56 +0300

    feat: use debian 13
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/ec2.tf
+++ terraform/prod/ec2.tf
@@ -40,7 +40,7 @@ resource "aws_iam_instance_profile" "this" {
 }
 
 resource "aws_instance" "this" {
-  ami                         = "ami-051eaec1417c5d4ae"
+  ami                         = "ami-0da1f66573556d917"
   # ami                         = "ami-0b5ef45933f8fa37d"
   instance_type               = "t3.medium"
   subnet_id                   = aws_subnet.public_subnet_a.id
