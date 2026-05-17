ccce0282f9f0285795ad70a628ede66f255c7e51
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-05-17 22:24:01 +0300

    chore: try without pipefail
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/ec2.tf
+++ terraform/prod/ec2.tf
@@ -41,7 +41,7 @@ resource "aws_iam_instance_profile" "this" {
 
 resource "aws_instance" "this" {
   # ami                         = "ami-0da1f66573556d917" # Debian 13
-  ami                         = "ami-051eaec1417c5d4ae" # Ubuntu 26.04
+  ami                         = "ami-051eaec1417c5d4ae" # Debian 13
   instance_type               = "t3.medium"
   subnet_id                   = aws_subnet.public_subnet_a.id
   vpc_security_group_ids      = [aws_security_group.this.id]
@@ -50,7 +50,7 @@ resource "aws_instance" "this" {
   user_data                   = <<-EOF
     #!/bin/bash
 
-    set -euo pipefail
+    # set -euo pipefail
 
     # =============================================================================
     # STEP 1: CORE DEPENDENCIES 
@@ -103,6 +103,140 @@ resource "aws_instance" "this" {
     fi
 
     tailscale up --authkey="$AUTH_KEY" --hostname="${local.org}-${local.env}-work-01" --advertise-tags="$TS_TAGS" --ssh
+
+    # =============================================================================
+    # KUBERNETES CLUSTER BOOTSTRAP
+    # Refs:
+    #   - https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm
+    #   - https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm
+    #   - https://kubernetes.io/docs/setup/production-environment/container-runtimes
+    #   - https://containerd.io/releases/#kubernetes-support
+    #   - https://docs.cilium.io/en/latest/installation/k8s-install-kubeadm
+    #   - https://max-pfeiffer.github.io/installing-kubernetes-on-debian-13-trixie.html
+    # =============================================================================
+
+    K8S_MAJOR="1"
+    K8S_MINOR="35"
+    K8S_PATCH="5"
+    CILIUM_VERSION="1.19.4"
+
+    # =============================================================================
+    # STEP 2: CRI (CONTAINERD)
+    # =============================================================================
+
+    # Disable swap - kubelet will fail to start if swap is enabled
+    # To make the changes permanent adjust the /etc/fstab file
+    # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#swap-configuration
+    swapoff -a
+
+    # Prepare the kernel for container networking by:
+    # - Load needed kernel drivers
+    # - Apply proper kernel parameters
+    modprobe overlay
+    modprobe br_netfilter
+    cat <<MODULES > /etc/modules-load.d/k8s.conf
+    overlay
+    br_netfilter
+    MODULES
+
+    cat <<SYSCTL > /etc/sysctl.d/k8s.conf
+    net.ipv4.ip_forward = 1
+    net.bridge.bridge-nf-call-iptables = 1
+    net.bridge.bridge-nf-call-ip6tables = 1
+    SYSCTL
+    sysctl --system
+
+    # Install and configure containerd
+    # https://docs.docker.com/engine/install/debian/#install-using-the-repository
+    apt update -y
+    apt install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt update -y
+    apt install -y containerd.io
+    containerd config default | tee /etc/containerd/config.toml
+    sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+    systemctl enable containerd
+    systemctl restart containerd
+    systemctl status containerd
+
+    # =============================================================================
+    # STEP 3: HELM 
+    # =============================================================================
+
+    curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    apt update
+    apt install -y helm
+
+    # =============================================================================
+    # STEP 4: KUBEADM + KUBELET + KUBECTL
+    # =============================================================================
+    curl -fsSL "https://pkgs.k8s.io/core:/stable:/v$K8S_MAJOR.$K8S_MINOR/deb/Release.key" | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$K8S_MAJOR.$K8S_MINOR/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+    apt-get update
+    apt-get install -y kubelet=$K8S_MAJOR.$K8S_MINOR.$K8S_PATCH-1.1 kubeadm=$K8S_MAJOR.$K8S_MINOR.$K8S_PATCH-1.1 kubectl=$K8S_MAJOR.$K8S_MINOR.$K8S_PATCH-1.1
+    apt-mark hold kubelet kubeadm kubectl
+
+    # =============================================================================
+    # STEP 5: KUBEADM INIT 
+    # =============================================================================
+    echo "$(hostname -i)  k8s-endpoint" >> /etc/hosts
+    kubeadm init --kubernetes-version $K8S_MAJOR.$K8S_MINOR.$K8S_PATCH --control-plane-endpoint k8s-endpoint
+    mkdir -p $HOME/.kube
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown $(id -u):$(id -g) $HOME/.kube/config
+
+		until kubectl get --raw /readyz &>/dev/null; do sleep 5; done
+    NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
+
+    # Allow scheduling on control-plane (single-node cluster)
+    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+    # =============================================================================
+    # STEP 6: CNI (CILIUM) 
+    # =============================================================================
+
+    helm repo add cilium https://helm.cilium.io
+    helm install cilium cilium/cilium \
+        --version $CILIUM_VERSION \
+        --namespace kube-system \
+        --set kubeProxyReplacement=true \
+        --set operator.replicas=1 \
+        --set hubble.relay.enabled=true \
+        --set hubble.ui.enabled=true
+
+    CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+    CLI_ARCH=amd64
+    curl -L --fail --remote-name-all "https://github.com/cilium/cilium-cli/releases/download/$CILIUM_CLI_VERSION/cilium-linux-$(dpkg --print-architecture).tar.gz{,.sha256sum}"
+    sha256sum --check "cilium-linux-$(dpkg --print-architecture).tar.gz.sha256sum"
+    tar xzvfC "cilium-linux-$(dpkg --print-architecture).tar.gz" /usr/local/bin
+    rm cilium-linux-$(dpkg --print-architecture).tar.gz{,.sha256sum}
+
+    cilium status --wait
+
+    # =============================================================================
+    # STEP 7: BOOTSTRAP WITH FLUXCD
+    # =============================================================================
+
+    curl -s https://fluxcd.io/install.sh | bash
+
+    kubectl create namespace doppler-operator-system
+    kubectl create secret generic doppler-token-secret -n doppler-operator-system --from-literal=serviceToken=${var.dp_token}
+
+    GITHUB_TOKEN=${var.gh_access_token} flux bootstrap github \
+        --token-auth=true \
+        --owner=${local.org} \
+        --repository=infra \
+        --branch=main \
+        --path=k8s/overlays/${local.env}/shoot-work-01 \
+        --read-write-key=true \
+        --personal=false \
+        --timeout=10m
   EOF
 
   root_block_device {
