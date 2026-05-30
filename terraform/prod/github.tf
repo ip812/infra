f60586a5d378e987e8203a83b86811fb8622c8c2
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-05-30 22:21:32 +0300

    feat: move all vars to github
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/github.tf
+++ terraform/prod/github.tf
@@ -43,6 +43,66 @@ locals {
       secret_name = "CF_ACCOUNT_ID"
       value       = var.cf_account_id
     }
+    infra_AWS_ACCESS_KEY_ID = {
+      repository  = "infra"
+      secret_name = "AWS_ACCESS_KEY_ID"
+      value       = var.aws_access_key_id
+    }
+    infra_AWS_SECRET_ACCESS_KEY = {
+      repository  = "infra"
+      secret_name = "AWS_SECRET_ACCESS_KEY"
+      value       = var.aws_secret_access_key
+    }
+    infra_CF_API_TOKEN = {
+      repository  = "infra"
+      secret_name = "CF_API_TOKEN"
+      value       = var.cf_api_token
+    }
+    infra_CF_IP812_ZONE_ID = {
+      repository  = "infra"
+      secret_name = "CF_IP812_ZONE_ID"
+      value       = var.cf_ip812_zone_id
+    }
+    infra_CF_TUNNEL_SECRET = {
+      repository  = "infra"
+      secret_name = "CF_TUNNEL_SECRET"
+      value       = var.cf_tunnel_secret
+    }
+    infra_TS_TAILNET = {
+      repository  = "infra"
+      secret_name = "TS_TAILNET"
+      value       = var.ts_tailnet
+    }
+    infra_PGADMIN_EMAIL = {
+      repository  = "infra"
+      secret_name = "PGADMIN_EMAIL"
+      value       = var.pgadmin_email
+    }
+    infra_PGADMIN_PASSWORD = {
+      repository  = "infra"
+      secret_name = "PGADMIN_PASSWORD"
+      value       = var.pgadmin_password
+    }
+    infra_PG_USERNAME = {
+      repository  = "infra"
+      secret_name = "PG_USERNAME"
+      value       = var.pg_username
+    }
+    infra_PG_PASSWORD = {
+      repository  = "infra"
+      secret_name = "PG_PASSWORD"
+      value       = var.pg_password
+    }
+    infra_FD_EMAIL_1 = {
+      repository  = "infra"
+      secret_name = "FD_EMAIL_1"
+      value       = var.fd_email_1
+    }
+    infra_FD_EMAIL_2 = {
+      repository  = "infra"
+      secret_name = "FD_EMAIL_2"
+      value       = var.fd_email_2
+    }
     lambdas_AWS_ACCESS_KEY_ID = {
       repository  = "lambdas"
       secret_name = "AWS_ACCESS_KEY_ID"
