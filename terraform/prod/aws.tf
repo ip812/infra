b4c4652ec6da5d1a0d133c42332e2cb3cc9a15bf
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-05-31 00:10:26 +0300

    chore: simlify terraform files
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/aws.tf
+++ terraform/prod/aws.tf
@@ -1,3 +1,37 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = local.aws_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = local.default_tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags   = local.default_tags
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(local.aws_vpc_cidr, 8, 1)
+  availability_zone       = local.aws_az_a
+  map_public_ip_on_launch = true
+  tags                    = local.default_tags
+}
+
+resource "aws_route_table" "public_subnet_a_rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = local.default_tags
+}
+
+resource "aws_route_table_association" "public_subnet_a_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_subnet_a_rt.id
+}
+
 resource "aws_security_group" "this" {
   vpc_id = aws_vpc.vpc.id
 
@@ -99,7 +133,6 @@ resource "aws_instance" "this" {
     tailscale up --authkey="$AUTH_KEY" --hostname="${local.org}-${local.env}-work-01" --advertise-tags="$TS_TAGS" --ssh
 
     # Trigger kubeadm-init Ansible playbook
-    # foo
     curl -fsSL -X POST \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${var.gh_access_token}" \
