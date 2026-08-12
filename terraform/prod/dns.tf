230dc2da679c01ea970a065682a85473b1383c6a
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-08-12 23:52:14 +0300

    feat: rm domains from cloudflare, accessible from wireguard now
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/prod/dns.tf
+++ terraform/prod/dns.tf
@@ -1,28 +1,7 @@
 locals {
   route_config = {
-    proxmox = {
-      is_protected = false
-      proxied      = false
-      ip           = var.home_public_ip
-    }
     blog = {
       is_protected = false
-      tunnel       = cloudflare_zero_trust_tunnel_cloudflared.cf_shoot_work_01_tunnel
-    }
-    pgadmin = {
-      is_protected = true
-      tunnel       = cloudflare_zero_trust_tunnel_cloudflared.cf_shoot_work_01_tunnel
-    }
-    victoria-logs = {
-      is_protected = true
-      tunnel       = cloudflare_zero_trust_tunnel_cloudflared.cf_shoot_work_01_tunnel
-    }
-    victoria-traces = {
-      is_protected = true
-      tunnel       = cloudflare_zero_trust_tunnel_cloudflared.cf_shoot_work_01_tunnel
-    }
-    cilium-hubble = {
-      is_protected = true
       tunnel       = cloudflare_zero_trust_tunnel_cloudflared.cf_shoot_work_01_tunnel
     }
   }
